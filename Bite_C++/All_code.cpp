bc6bc50cad257768de8d155611906a426a8524f8
rudwo4861@naver.com <rudwo4861@naver.com>
2023-11-01 17:21:39 +0900

    날짜 계산기 드디어 완료!!

--- Bite_C++/All_code.cpp
+++ Bite_C++/All_code.cpp
@@ -147,95 +147,167 @@
 //	delete[] list;
 //}
 
+//#include <iostream>
+//#include <string>
+//struct Animal
+//{
+//	std::string name;
+//	int age;
+//	int health;
+//	int food;
+//	int clean;
+//};
+//
+//void create_animal(Animal* animal)
+//{
+//	std::cout << "동물의 이름: ";
+//	std::cin >> animal->name;
+//
+//	std::cout << "동물의 나이: ";
+//	std::cin >> animal->age;
+//
+//	animal->health = 100;
+//	animal->food = 100;
+//	animal->clean = 50;
+//}
+//
+//void play(Animal* animal)
+//{
+//	animal->health += 10;
+//	animal->food -= 20;
+//	animal->clean -= 30;
+//}
+//
+//void one_day_pass(Animal* animal)
+//{
+//	animal->health -= 10;
+//	animal->food -= 30;
+//	animal->clean -= 20;
+//}
+//
+//void show_stat(Animal* animal)
+//{
+//	std::cout << animal->name << "의 상태 " << std::endl;
+//	std::cout << "체력    : " << animal->health << std::endl;
+//	std::cout << "배부름    : " << animal->food << std::endl;
+//	std::cout << "청결    : " << animal->clean << std::endl;
+//}
+//int main()
+//{
+//	Animal* list[10];
+//	int animal_num = 0;
+//
+//	for (;;)
+//	{
+//		std::cout << "1. 동물 추가하기" << std::endl;
+//		std::cout << "2. 놀기" << std::endl;
+//		std::cout << "3. 상태 보기" << std::endl;
+//		std::cout << "4. 그만하기" << std::endl;
+//		int input;
+//		std::cin >> input;
+//
+//		switch (input)
+//		{
+//			int play_with;
+//		case 1:
+//			list[animal_num] = new Animal;
+//			create_animal(list[animal_num]);
+//
+//			animal_num++;
+//			break;
+//
+//		case 2:
+//			std::cout << "누구랑 놀게? : ";
+//			std::cin >> play_with;
+//
+//			if (play_with < animal_num) play(list[play_with]);
+//			break;
+//		case 3:
+//			std::cout << "누구껄 보게? : ";
+//			std::cin >> play_with;
+//			if (play_with < animal_num) show_stat(list[play_with]);
+//			break;
+//		}
+//
+//		if (input == 4)
+//		{
+//			std::cout << "그만 놀자" << std::endl;
+//			break;
+//		}
+//		for (int i = 0; i != animal_num; ++i)
+//		{
+//			one_day_pass(list[i]);
+//		}
+//	}
+//	for (int i = 0; i != animal_num; ++i)
+//	{
+//		delete list[i];
+//	}
+//}
+
 #include <iostream>
-#include <string>
-struct Animal
+
+class Date
 {
-	std::string name;
-	int age;
-	int health;
-	int food;
-	int clean;
+	int year_;
+	int month_;
+	int day_;
+	int month_array[13] = { 0, 31, 28, 31,30,31,30,31,31,30,31,30,31 };
+
+public:
+	Date(int year, int month, int day);
+	void add_day(int inc);
+	void add_month(int inc);
+	void add_year(int inc);
+
+	void show_data();
 };
 
-void create_animal(Animal* animal)
+Date::Date(int year, int month, int day) : year_(year), month_(month), day_(day)
 {
-	std::cout << "동물의 이름: ";
-	std::cin >> animal->name;
-
-	std::cout << "동물의 나이: ";
-	std::cin >> animal->age;
-
-	animal->health = 100;
-	animal->food = 100;
-	animal->clean = 50;
+	
 }
 
-void play(Animal* animal)
+void Date::add_day(int inc)
 {
-	animal->health += 10;
-	animal->food -= 20;
-	animal->clean -= 30;
+	day_ = (day_ + inc);
+
+	while (day_ > month_array[month_])
+	{
+		day_ -= month_array[month_];
+		add_month(1);
+
+		if ((year_ % 4 == 0 || year_ % 400 == 0) && year_ % 100 != 0)
+			month_array[2] = 29;
+		else
+			month_array[2] = 28;
+	}
 }
 
-void one_day_pass(Animal* animal)
+void Date::add_month(int inc)
 {
-	animal->health -= 10;
-	animal->food -= 30;
-	animal->clean -= 20;
+	month_ = month_ + inc;
+	while (month_ > 12)
+	{
+		month_ -= 12;
+		year_++;
+	}	
 }
 
-void show_stat(Animal* animal)
+void Date::add_year(int inc)
 {
-	std::cout << animal->name << "의 상태 " << std::endl;
-	std::cout << "체력    : " << animal->health << std::endl;
-	std::cout << "배부름    : " << animal->food << std::endl;
-	std::cout << "청결    : " << animal->clean << std::endl;
+	year_ += inc;
 }
+
+void Date::show_data()
+{
+	std::cout << "현재 년도 " << year_ << '\n' << "현재 월 " << month_ << '\n' << "현재 일 " << day_ << std::endl;
+}
+
 int main()
 {
-	Animal* list[10];
-	int animal_num = 0;
-
-	for (;;)
-	{
-		std::cout << "1. 동물 추가하기" << std::endl;
-		std::cout << "2. 놀기" << std::endl;
-		std::cout << "3. 상태 보기" << std::endl;
-
-		int input;
-		std::cin >> input;
-
-		switch (input)
-		{
-			int play_with;
-		case 1:
-			list[animal_num] = new Animal;
-			create_animal(list[animal_num]);
-
-			animal_num++;
-			break;
-
-		case 2:
-			std::cout << "누구랑 놀게? : ";
-			std::cin >> play_with;
-
-			if (play_with) play(list[play_with]);
-			break;
-		case 3:
-			std::cout << "누구껄 보게? : ";
-			std::cin >> play_with;
-			if (play_with < animal_num) show_stat(list[play_with]);
-			break;
-		}
-
-		for (int i = 0; i != animal_num; ++i)
-		{
-			one_day_pass(list[i]);
-		}
-	}
-	for (int i = 0; i != animal_num; ++i)
-	{
-		delete list[i];
-	}
+	Date date(2001,11,12);
+	date.add_day(8024);
+	date.show_data();
 }
+
